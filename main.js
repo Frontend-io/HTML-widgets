68d2f4983213ea3132470783a776d9afc46a2d33
Jefferson Osagie Iyobosa <iyobosajefferson2@gmail.com>
2019-07-18 23:30:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+// Custom select dropdown
+	// *********************
+	window.addEventListener("load", customSelect)
+	function customSelect(){
+		// Display the options again after closure
+		container = document.querySelectorAll(".select-wrap");
+		options = document.querySelectorAll(".select-wrap input");
+		siblings = [];
+		for (var n = 0; n < container.length; n++) {
+			container[n].addEventListener("click", restore);
+			function restore(){
+				this.children[1].classList.remove("hide")
+			};
+
+		};
+
+		// Update the selection
+		for (var i = 0; i < options.length; i++) {
+			siblings.push(options[i]);
+			options[i].addEventListener('change', choose);
+			function choose(){
+				for (var j = 0; j < siblings.length; j++) {
+					siblings[j].parentElement.classList.remove("selected")
+				};
+				this.parentElement.classList.add("selected");
+				this.parentElement.parentElement.parentElement.firstElementChild.children[0].value = this.value;
+				this.parentElement.parentElement.classList.add("hide");
+			}
+		};
+	}
+
